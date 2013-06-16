771b2949410834f92bd5f6d86b960fd9bda51ed0
Maciej Nowak <maciejt.nowak@gmail.com>
2013-06-16 21:50:17 +0200

    Problem 7 solved

--- problem7.erl
+++ problem7.erl
@@ -6,11 +6,28 @@
 %% API
 -export([]).
 
-erasto(N) ->
-        N.
+divisableBy(_, []) ->
+        false;
+divisableBy(N, [Num | _])
+        when N rem Num =:= 0 ->
+        true;
+divisableBy(N, [Num | Nums])
+        when N rem Num =/= 0 ->
+        divisableBy(N, Nums).
 
-solve(N)->
-        N.
 
-main(_)->
-        io:format("~p\n", [solve(6)]).
+solve(N) ->
+        solve(2, N, []).
+
+solve(Current, 0, _) ->
+        Current - 1;
+solve(Current, Remaining, AlreadyFound) ->
+        case divisableBy(Current, AlreadyFound) of
+                true -> solve(Current + 1, Remaining, AlreadyFound);
+                false -> solve(Current + 1, Remaining - 1, AlreadyFound ++ [Current])
+        end.
+
+
+
+main(_) ->
+        io:format("~p\n", [solve(10001)]).
